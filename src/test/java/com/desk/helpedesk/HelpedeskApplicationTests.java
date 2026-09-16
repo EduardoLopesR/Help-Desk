eb57d7ae1703e6e8f69650445adb7e9b24f52cf5
edulopesresende19@gmail.com <edulopesresende19@gmail.com>
2026-09-16 11:03:40 -0300

    Projeto inicial

---
+++ src/test/java/com/desk/helpedesk/HelpedeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desk.helpedesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpedeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
