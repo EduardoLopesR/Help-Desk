2ba2b68794773a8128463d6141626eae76814db8
edulopesresende19@gmail.com <edulopesresende19@gmail.com>
2026-09-16 11:32:26 -0300

    Tipos de Enums criados

---
+++ src/main/java/com/desk/helpedesk/domain/enums/Prioridade.java
@@ -0,0 +1,36 @@
+package com.desk.helpedesk.domain.enums;
+
+public enum Prioridade {
+	
+	BAIXA(0, "BAIXA"), MEDIA(1, "MEDIA"), ALTA(2, "ALTA");
+	
+	private Integer codigo;
+	private String descricao;
+	
+	Prioridade(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	@SuppressWarnings("unused")
+	public static Prioridade toEnum(Integer codigo) {
+		if(codigo == null) {
+			if (codigo == null) {
+			return null;
+		}
+		
+		for(Prioridade p : Prioridade.values()) {
+			if (codigo.equals(p.getCodigo())) {
+				return p;
+			}
+			}
+		}
+		throw new IllegalArgumentExcepetion("Prioridade inváida");
+	}
+}
